7c9ded833b781ddc7bd14ae604d4b871f30717e0
Ejiofor <nwofor@ymail.com>
2020-10-27 00:01:55 +0100

    (cards-component):second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts  = [
+    {title:'Neat tree',
+     imageUrl: 'assets/tree.jpeg',
+     username: 'nature',
+     content: 'I saw this neat tree today'},
+    
+     {title:'Mountain Biking',
+     imageUrl: 'assets/biking.jpeg',
+     username: 'biking1222',
+     content: 'biking is great today'},
+
+     {title:'Snowy mountain',
+     imageUrl: 'assets/mountain.jpeg',
+     username: 'mountain lover',
+     content: 'Here is a picture of a snow maountain'}
+  ];
 }
